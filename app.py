3e8abb08a625333359f54f76a386a3a99bf2e761
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-10-01 12:05:59 +0700

    Fix CORS

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -8,6 +9,8 @@ def create_app():
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flask_blog_site'
   app.config['JWT_SECRET_KEY'] = 'FLASK_APP_KEY'
+  
+  CORS(app)
 
   db.init_app(app)
   from routes import register_app
